346f87c612356353fbe3692d2120e404511ec40d
mamilaaw <moak321@gmail.com>
2023-02-26 21:22:33 +0300

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,6 +1,6 @@
 <?php
 //a
-define('BOT_TOKEN', '5769796666:AAG1yHG4ujMTgV19OqPpacWMc6_6NNRB6VA');
+define('BOT_TOKEN', '');// The bot token here
 define('COMMON_VOICE_API_URL',"https://dev.voice.mozit.cloud/api/v1");
 //define('IMGBB_TOKEN', '039346d5e7b54d3a7ecb6a67a84e994d');
 define('ADMIN_ID',array(1098187878,255921944));// ); 1098187878  308325853
