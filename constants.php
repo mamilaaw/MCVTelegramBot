c48a999c8ddf34d5c8229c988a43ef531b73d66a
mamilaaw <moak321@gmail.com>
2023-06-25 16:04:33 +0300

    Update constants.php

--- constants.php
+++ constants.php
@@ -5,6 +5,6 @@ define('BOT_TOKEN', 'YOUR_BOT_TOKEN');
 //https://dev.voice.mozit.cloud/api/v1
 define('COMMON_VOICE_API_URL',"https://commonvoice.mozilla.org/api/v1");
 define('LANGUAGE', 'en');
-define('ADMIN_ID',array(11111111));// ); 1098187878  308325853
+define('ADMIN_ID',array(11111111));// The admin telegram id
 
 
