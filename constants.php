3eb57ada0e0d81fe7b4d3fe6a90dd032a14f53b5
mamilaaw <moak321@gmail.com>
2023-06-25 16:03:55 +0300

    Update constants.php
    Use your own credentials on these

--- constants.php
+++ constants.php
@@ -1,11 +1,10 @@
 <?php
 //a
-define('BOT_TOKEN', '5769796666:AAG1yHG4ujMTgV19OqPpacWMc6_6NNRB6VA');
+define('BOT_TOKEN', 'YOUR_BOT_TOKEN');
 //https://commonvoice.mozilla.org/api/v1
 //https://dev.voice.mozit.cloud/api/v1
 define('COMMON_VOICE_API_URL',"https://commonvoice.mozilla.org/api/v1");
 define('LANGUAGE', 'en');
-//define('IMGBB_TOKEN', '039346d5e7b54d3a7ecb6a67a84e994d');
-define('ADMIN_ID',array(1098187878,255921944));// ); 1098187878  308325853
+define('ADMIN_ID',array(11111111));// ); 1098187878  308325853
 
 
